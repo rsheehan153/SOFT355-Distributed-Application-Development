3b030b3c8dc4b675171bafe8bc86ffe8ecd327b5
Ryan Sheehan <ryan.sheehan@plymouth.ac.uk>
2020-01-02 19:14:47 +0000

    implementing fix for pipeline

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.post('/room', (req, res) => {
   
   rooms[req.body.room] = { users: {} }
   res.redirect(req.body.room)
+  // Send message that new room was created
   io.emit('room-created', req.body.room)
 })
 
@@ -36,18 +37,15 @@ app.get('/:room', (req, res) => {
 
 
 io.on('connection', socket => {
-  socket.on('new-user', (room, name) => 
-  {
+  socket.on('new-user', (room, name) => {
     socket.join(room)
     rooms[room].users[socket.id] = name
     socket.to(room).broadcast.emit('user-connected', name)
   })
-  socket.on('send-chat-message', (room, message) => 
-  {
+  socket.on('send-chat-message', (room, message) => {
     socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
   })
-  socket.on('disconnect', () => 
-  {
+  socket.on('disconnect', () => {
     getUserRooms(socket).forEach(room => {
       socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
       delete rooms[room].users[socket.id]
@@ -55,10 +53,8 @@ io.on('connection', socket => {
   })
 })
 
-function getUserRooms(socket) 
-{
-  return Object.entries(rooms).reduce((names, [name, room]) => 
-  {
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
     if (room.users[socket.id] != null) names.push(name)
     return names
   }, [])
